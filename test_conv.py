9010ba4534dcc77a7ae07601a0e60bd08985833b
aeryskyB <79935164+aeryskyB@users.noreply.github.com>
2025-01-09 11:30:37 +0530

    add better testcases for conv; checked with torch

--- test_conv.py
+++ test_conv.py
@@ -1,10 +1,9 @@
+import numpy as np
 import torch
 from base.tensor import Tensor
 from base.compose import Conv2d
 from base.dist import Randn
 from time import perf_counter
-
-# WARNING: need better tests
 
 print("simple convolution...")
 
@@ -15,17 +14,20 @@ b1 = c1(a1)
 end = perf_counter()
 print(f"torch time: {end - start:.3}s")
 
-a2 = Randn((4, 16, 16))
+a2 = Tensor(a1.detach().numpy())
 c2 = Conv2d(4, 10, 3)
+c2.weight.val = c1.weight.detach().numpy().copy()
+c2.bias.val = c1.bias.detach().numpy().copy()
+
 start = perf_counter()
 b2 = c2(a2)
 end = perf_counter()
 print(f"d time: {end - start:.3}s (expecting worse perf)")
 
 assert a1.shape == a2.shape and c1.weight.shape == c2.weight.shape, b1.shape == b2.shape
+assert np.allclose(b2.numpy(), b1.detach().numpy(), atol=1e-6)
 
 ##############################################
-
 print("\ngrouped convolution...")
 c1 = torch.nn.Conv2d(4, 10, 3, groups=2)
 start = perf_counter()
@@ -34,12 +36,16 @@ end = perf_counter()
 print(f"torch time: {end - start:.3}s")
 
 c2 = Conv2d(4, 10, 3, groups=2)
+c2.weight.val = c1.weight.detach().numpy().copy()
+c2.bias.val = c1.bias.detach().numpy().copy()
+
 start = perf_counter()
 b2 = c2(a2)
 end = perf_counter()
 print(f"d time: {end - start:.3}s")
 
 assert a1.shape == a2.shape and c1.weight.shape == c2.weight.shape, b1.shape == b2.shape
+assert np.allclose(b2.numpy(), b1.detach().numpy(), atol=1e-6)
 
 ##############################################
 
@@ -51,12 +57,16 @@ end = perf_counter()
 print(f"torch time: {end - start:.3}s")
 
 c2 = Conv2d(4, 10, 3, dilation=2)
+c2.weight.val = c1.weight.detach().numpy().copy()
+c2.bias.val = c1.bias.detach().numpy().copy()
+
 start = perf_counter()
 b2 = c2(a2)
 end = perf_counter()
 print(f"d time: {end - start:.3}s")
 
 assert a1.shape == a2.shape and c1.weight.shape == c2.weight.shape, b1.shape == b2.shape
+assert np.allclose(b2.numpy(), b1.detach().numpy(), atol=1e-6)
 
 ##############################################
 
@@ -71,11 +81,15 @@ b1 = c1(a1)
 end = perf_counter()
 print(f"torch time: {end - start:.3}s")
 
-a2 = Randn((1_000, 3, 16, 16))
+a2 = Tensor(a1.detach().numpy())
 c2 = Conv2d(3, 10, 3)
+c2.weight.val = c1.weight.detach().numpy().copy()
+c2.bias.val = c1.bias.detach().numpy().copy()
+
 start = perf_counter()
 b2 = c2(a2)
 end = perf_counter()
 print(f"d time (shameful perfomance TT): {end - start:.3}s")
 
 assert a1.shape == a2.shape and c1.weight.shape == c2.weight.shape, b1.shape == b2.shape
+assert np.allclose(b2.numpy(), b1.detach().numpy(), atol=1e-6)
