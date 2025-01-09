60b332d29473fc990298039090b3c422e211ca84
aeryskyB <79935164+aeryskyB@users.noreply.github.com>
2025-01-09 10:58:51 +0530

    add dilated and grouped conv; need better testcases

--- base/compose.py
+++ base/compose.py
@@ -5,33 +5,42 @@ from collections.abc import Iterable
 from .tensor import Tensor
 
 class Linear:
-    def __init__(self, in_size: int, out_size: int, need = False):
+    def __init__(self, in_size: int, out_size: int, need = False) -> None:
         t = np.sqrt(1/in_size)
         self.weight = Uniform(low=-t, high=t, shape=(out_size, in_size), need=need)
         self.bias = Uniform(low=-t, high=t, shape=(out_size,), need=need)
         self.params = {"weight" : self.weight, "bias" : self.bias}
 
-    def __call__(self, x: Tensor):
+    def __call__(self, x: Tensor) -> Union[Tensor, AssertionError]:
+        assert x.shape[-1] == self.weight.shape[1], f"Expected input tensor shape: (..., {self.weight.shape[1]}), given: (..., {x.shape[-1]})"
         return self.bias + x @ self.weight.T
 
 class Conv2d:
-    def __init__(self, in_channels: int, out_channels, kernel_size: Iterable, stride: Union[int, Iterable] = 1, pad: [int, Iterable] = 0, pad_val = 0, need = False):
+    def __init__(self, in_channels: int, out_channels, kernel_size: Iterable, stride: Union[int, Iterable] = 1, pad: Union[int, Iterable] = 0, pad_val = 0, dilation: Union[int, Iterable] = 1, groups: int = 1, need = False) -> Union[None, AssertionError]:
+        assert out_channels % groups == 0 and in_channels % groups == 0, "groups must be a common factor for both in_channels and out_channels"
         self.in_channels = in_channels
         self.out_channels = out_channels
-        self.kernel_size = (kernel_size, kernel_size) if isinstance(kernel_size, int) else tuple(kernel_size)
-        self.weight = Randn((out_channels, in_channels, *self.kernel_size), need=need)      # initialization will come from different distribution (Kaiming and others?)
-        self.bias = Randn((out_channels,), need=need)                                       # ...
         self.stride = (stride, stride) if isinstance(stride, int) else tuple(stride)
         self.pad = (pad, pad) if isinstance(pad, int) else tuple(pad)
         self.pad_val = pad_val
+        self.dilation = (dilation, dilation) if isinstance(dilation, int) else tuple(dilation)
+        self.kernel_size = (kernel_size, kernel_size) if isinstance(kernel_size, int) else tuple(kernel_size)
+        self.groups = groups
+
+        self.in_channels_eff = self.in_channels // self.groups
+        self.out_channels_eff = self.out_channels // self.groups
+
+        t = np.sqrt(self.groups/(self.in_channels * self.kernel_size[0] * self.kernel_size[1]))
+        self.weight = Uniform(low=-t, high=t, shape=(self.out_channels, self.in_channels_eff, *self.kernel_size), need=need)
+        self.bias = Uniform(low=-t, high=t, shape=(self.out_channels,), need=need)
         self.params = {"weight" : self.weight, "bias" : self.bias}
 
-    def __call__(self, x: Tensor):
+    def __call__(self, x: Tensor) -> Union[Tensor, AssertionError]:
         assert len(x.shape) >= 3, "Convolution permitted only for 3D+ tensors"
-        assert x.shape[-2] + self.pad[0] >= self.kernel_size[0] and x.shape[-1] + self.pad[1] >= self.kernel_size[1], f"Kernel size exceeds effective input size"
+        assert x.shape[-2] + self.pad[0] >= self.kernel_size[0] and x.shape[-1] + self.pad[1] >= self.kernel_size[1], "Kernel size exceeds effective input size"
 
-        h = (x.shape[-2] - self.kernel_size[0] + 2*self.pad[0]) // self.stride[0] + 1
-        w = (x.shape[-1] - self.kernel_size[1] + 2*self.pad[1]) // self.stride[1] + 1
+        h = (x.shape[-2] - (self.kernel_size[0] - 1) * self.dilation[0] + 2 * self.pad[0] - 1) // self.stride[0] + 1
+        w = (x.shape[-1] - (self.kernel_size[1] - 1) * self.dilation[1] + 2 * self.pad[1] - 1) // self.stride[1] + 1
         out_shape = (*x.shape[:-3], self.out_channels, h, w) if len(x.shape) > 3 else (self.out_channels, h, w)
 
         num_batch_flat = 1
@@ -39,17 +48,20 @@ class Conv2d:
         out_shape_batch_flat = (num_batch_flat, *out_shape[-3:])
 
         out = Zeros(out_shape_batch_flat)
-        x_ = Zeros((num_batch_flat, x.shape[-3], x.shape[-2]+2*self.pad[0], x.shape[-1]+2*self.pad[1]))
-        x_ = x_ + self.pad_val
+        x_ = Zeros((num_batch_flat, x.shape[-3], x.shape[-2]+2*self.pad[0], x.shape[-1]+2*self.pad[1])) + self.pad_val
         x_[..., self.pad[0]:self.pad[0]+x.shape[-2], self.pad[1]:self.pad[1]+x.shape[-1]] = x
 
-        for b in range(out.shape[0]):
+        for b in range(out.shape[0]):   # batch
             for c in range(self.out_channels):
-                for i in range(0, x.shape[-2] - self.kernel_size[0] + 2*self.pad[0] + self.stride[0], self.stride[0]):
+                g = c // self.out_channels_eff
+                for i in range(0, x.shape[-2] - (self.kernel_size[0]-1)*self.dilation[0] + 2*self.pad[0] + self.stride[0] - 1, self.stride[0]):
                     idx_i = i // self.stride[0]
-                    for j in range(0, x.shape[-1] - self.kernel_size[1] + 2*self.pad[1] + self.stride[1], self.stride[1]):
+                    for j in range(0, x.shape[-1] - (self.kernel_size[1]-1)*self.dilation[1] + 2*self.pad[1] + self.stride[1] - 1, self.stride[1]):
                         idx_j = j // self.stride[1]
-                        out[b, c, idx_i, idx_j] = (self.weight[c,...] * x_[b, :, i:i+self.kernel_size[0], j:j+self.kernel_size[1]]).sum()
+                        out[b, c, idx_i, idx_j] = (self.weight[c,...] * x_[b, g*self.in_channels_eff:(g+1)*self.in_channels_eff, i:i+self.kernel_size[0]*self.dilation[0]:self.dilation[0], j:j+self.kernel_size[1]*self.dilation[1]:self.dilation[1]]).sum()
                 out[b, c,...] = out[b, c,...] + self.bias[c]
+
+
+        # circular-fft for conv?
 
         return out.reshape(out_shape)
--- test_conv.py
+++ test_conv.py
@@ -4,6 +4,8 @@ from base.compose import Conv2d
 from base.dist import Randn
 from time import perf_counter
 
+# WARNING: need better tests
+
 print("simple convolution...")
 
 a1 = torch.randn((4, 16, 16))
@@ -11,18 +13,53 @@ c1 = torch.nn.Conv2d(4, 10, 3)
 start = perf_counter()
 b1 = c1(a1)
 end = perf_counter()
-print(f'torch time: {end - start:.3}s')
+print(f"torch time: {end - start:.3}s")
 
 a2 = Randn((4, 16, 16))
 c2 = Conv2d(4, 10, 3)
 start = perf_counter()
 b2 = c2(a2)
 end = perf_counter()
-print(f'd time: {end - start:.3}s (expecting worse perf)')
+print(f"d time: {end - start:.3}s (expecting worse perf)")
 
 assert a1.shape == a2.shape and c1.weight.shape == c2.weight.shape, b1.shape == b2.shape
 
 ##############################################
+
+print("\ngrouped convolution...")
+c1 = torch.nn.Conv2d(4, 10, 3, groups=2)
+start = perf_counter()
+b1 = c1(a1)
+end = perf_counter()
+print(f"torch time: {end - start:.3}s")
+
+c2 = Conv2d(4, 10, 3, groups=2)
+start = perf_counter()
+b2 = c2(a2)
+end = perf_counter()
+print(f"d time: {end - start:.3}s")
+
+assert a1.shape == a2.shape and c1.weight.shape == c2.weight.shape, b1.shape == b2.shape
+
+##############################################
+
+print("\ndilated convolution...")
+c1 = torch.nn.Conv2d(4, 10, 3, dilation=2)
+start = perf_counter()
+b1 = c1(a1)
+end = perf_counter()
+print(f"torch time: {end - start:.3}s")
+
+c2 = Conv2d(4, 10, 3, dilation=2)
+start = perf_counter()
+b2 = c2(a2)
+end = perf_counter()
+print(f"d time: {end - start:.3}s")
+
+assert a1.shape == a2.shape and c1.weight.shape == c2.weight.shape, b1.shape == b2.shape
+
+##############################################
+
 # for batches
 a1 = torch.randn((1_000, 3, 16, 16))
 c1 = torch.nn.Conv2d(3, 10, 3)
@@ -32,13 +69,13 @@ print("\n(big) batched convolution...")
 start = perf_counter()
 b1 = c1(a1)
 end = perf_counter()
-print(f'torch time: {end - start:.3}s')
+print(f"torch time: {end - start:.3}s")
 
 a2 = Randn((1_000, 3, 16, 16))
 c2 = Conv2d(3, 10, 3)
 start = perf_counter()
 b2 = c2(a2)
 end = perf_counter()
-print(f'd time (shameful perfomance TT): {end - start:.3}s')
+print(f"d time (shameful perfomance TT): {end - start:.3}s")
 
 assert a1.shape == a2.shape and c1.weight.shape == c2.weight.shape, b1.shape == b2.shape
